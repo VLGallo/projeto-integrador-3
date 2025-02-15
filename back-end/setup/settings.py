f43050388d1555b0ff210fad804d4140b04425f1
Vanessa Leticia Gallo <96696812+VLGallo@users.noreply.github.com>
2025-02-15 12:10:31 -0300

    altera variaveis de ambiente para futuro deploy

--- back-end/setup/settings.py
+++ back-end/setup/settings.py
@@ -3,25 +3,27 @@ from pathlib import Path
 import dj_database_url
 import os
 import sys
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
+# Carrega variáveis do .env
+load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Diretório base do projeto
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Chave secreta
+SECRET_KEY = os.getenv("SECRET_KEY", "chave-padrao-para-dev")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+# Define o ambiente
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "ad7bd4e341f351571f09e189cc402521"
-#os.environ.get("SECRET_KEY")
+# Hosts permitidos
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(" ")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #os.environ.get("DEBUG","False").lower() == "true"
+# Configuração de CORS
+CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS if not DEBUG else ["http://localhost:8081", "http://127.0.0.1:8081"]
+CORS_ALLOW_ALL_ORIGINS = DEBUG  # Permite todas as origens apenas em dev
 
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -39,42 +41,6 @@ INSTALLED_APPS = [
     "gerenciador_de_pedidos.apps.GerenciadorDePedidosConfig",
 ]
 
-# Authentication backends
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',  # Para permitir login via username (se necessário)
-#     'autenticacao.backends.EmailBackend',         # Nosso EmailBackend customizado
-# ]
-
-CORS_ALLOW_METHODS = [
-    'GET',
-    'POST',
-    'PUT',
-    'PATCH',
-    'DELETE',
-    'OPTIONS'
-]
-
-REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    #     'rest_framework.authentication.BasicAuthentication',
-    # ],
-}
-
-#CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_HOSTS").split(" ")
-#CORS_ALLOWED_ORIGINS = ["http://localhost:8081", "http://127.0.0.1:8081"]
-
-
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-#ALLOWED_HOSTS = ["127.0.0.1", "http://localhost:8000","http://127.0.0.1:8000"]
-
-# Permitir CORS de qualquer origem
-CORS_ALLOW_ALL_ORIGINS = True
-
-# Permitir qualquer host (não recomendado em produção)
-ALLOWED_HOSTS = ['*']
-
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -88,6 +54,65 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = "setup.urls"
+WSGI_APPLICATION = "setup.wsgi.application"
+
+
+# Configuração do banco de dados
+DB_ENGINE = os.getenv("DB_ENGINE", "postgresql").lower()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    # Usa a URL de banco de dados para produção
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    # Configuração local (PostgreSQL ou MySQL)
+    if DB_ENGINE == "postgresql":
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.postgresql",
+                "NAME": os.getenv("POSTGRES_NAME", "pizzaria_db"),
+                "USER": os.getenv("POSTGRES_USER", ""),
+                "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
+                "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+                "PORT": os.getenv("POSTGRES_PORT", "5432"),
+                'OPTIONS': {
+                    'client_encoding': 'UTF8',
+                },
+            }
+        }
+    elif DB_ENGINE == "mysql":
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.mysql",
+                "NAME": os.getenv("MYSQL_NAME", "pizzaria_db"),
+                "USER": os.getenv("MYSQL_USER", ""),
+                "PASSWORD": os.getenv("MYSQL_PASSWORD", ""),
+                "HOST": os.getenv("MYSQL_HOST", "localhost"),
+                "PORT": os.getenv("MYSQL_PORT", "3306"),
+            }
+        }
+    else:
+        raise ValueError("DB_ENGINE inválido. Escolha 'postgresql' ou 'mysql'.")
+
+
+
+# Configuração específica para testes
+if "test" in sys.argv:
+    DATABASES["default"]["NAME"] = "test_pizzaria_db"  # Nome do banco temporário
+    DATABASES["default"]["USER"] = "root"
+    DATABASES["default"]["PASSWORD"] = ""
+    DATABASES["default"]["HOST"] = "localhost"
+    DATABASES["default"]["PORT"] = "3306"
+
+#opção sugerida pelo gpt pra usar sqlite de temporario
+# if "test" in sys.argv:
+#     DATABASES["default"] = {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "test_db.sqlite3",
+#     }
+
 
 TEMPLATES = [
     {
@@ -105,104 +130,16 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "setup.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "pizzaria_db",
-        "USER": "postgres",
-        "PASSWORD": "086002",
-        "HOST": "localhost",
-        "PORT": "5432",
-        'OPTIONS': {
-            'client_encoding': 'UTF8',
-        },
-    }
-}
-
-# para conectar no MySQL
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "pizzaria_db",
-# # Substituir com dados da criação do no banco local
-#         "USER": "root",
-#         "PASSWORD": "",
-#         # Substituir se estiver usando um servidor remoto
-#         "HOST": "localhost",
-#         "PORT": "3306",
-#     }
-# }
-
-#database_url = os.environ.get("DATABASE_URL")
-#DATABASES["default"] = dj_database_url.parse("postgresql://zerissi_banco_user:qddfrvzueuEa065PWzGZTfsoF9PywCfe@dpg-csnseei3esus73ehomqg-a.oregon-postgres.render.com/zerissi_banco")
-#DATABASES["default"] = dj_database_url.parse(database_url)
-
-# if database_url and "test" not in sys.argv:
-#     DATABASES["default"] = dj_database_url.parse(database_url)
-
-# Configuração específica para testes
-if "test" in sys.argv:
-    DATABASES["default"]["NAME"] = "test_pizzaria_db"  # Nome do banco temporário
-    DATABASES["default"]["USER"] = "root"
-    DATABASES["default"]["PASSWORD"] = ""  
-    DATABASES["default"]["HOST"] = "localhost"
-    DATABASES["default"]["PORT"] = "3306"
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = "pt-br"
-
-TIME_ZONE = "America/Sao_Paulo"
-
-USE_I18N = True
-
-USE_TZ = True
-
-CORS_ORIGIN_ALLOW_ALL = True
-
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+# Configuração de arquivos estáticos
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
+# Configuração do models
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Configuração de idioma e fuso horário
+LANGUAGE_CODE = "pt-br"
+TIME_ZONE = "America/Sao_Paulo"
+USE_I18N = True
+USE_TZ = True
