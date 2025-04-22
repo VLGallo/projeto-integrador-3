dbcfd3d244f523517abd70128e7cdc425cb4ef62
AndreRFBaT <andrerfbatista@gmail.com>
2025-04-21 22:16:17 -0300

    FEA: Add motoboy screen spec

---
+++ cypress/cypress/e2e/9-telaMotoboy.spec.cy.js
@@ -0,0 +1,17 @@
+describe("Teste de login", () => {
+    before(() => {
+      cy.visit(Cypress.config("baseUrl"));
+      cy.get('[data-testid="login-input"]').type("AstDest");
+      cy.get('[data-testid="senha-input"]').type("123");
+      cy.get('[data-testid="entrar-btn"]').click();
+      cy.saveSessionState();
+    });
+
+    it("Deve exibir modal de boas-vindas e fechar ao clicar", () => {
+        cy.get('[data-testid="motoboy-welcome-text"]').should("be.visible");
+        cy.get('[data-testid="modal-ok-btn"]').click();
+        cy.get('[data-testid="motoboy-welcome-text"]').should("not.exist");
+        cy.get('[aria-label="cancelar-pedido-btn"]').click();
+
+    });
+  });
